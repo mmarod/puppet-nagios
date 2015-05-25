c1a883223853d3ee2383ba06dca94a151ad40c4e
Michael Marod <michael@michaelmarod.com>
2015-05-25 21:42:05 +0000

    add inotify capability

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,18 +1,24 @@
 class nagios::params {
   case $::osfamily {
     'Debian': {
-      $plugin_path         = '/usr/lib/nagios/plugins'
-      $eventhandler_path   = '/usr/share/nagios3/plugins/eventhandlers'
-      $nagios_cfg_path     = '/etc/nagios3/nagios.cfg'
-      $confdir             = '/etc/nagios3/conf.d'
-      $confdir_hosts       = '/etc/nagios3/conf.d/hosts'
-      $nagios_service_name = 'nagios3'
-      $cfg_files           = [ '/etc/nagios3/commands.cfg' ]
-      $cfg_dirs            = [ '/etc/nagios-plugins/config', '/etc/nagios3/conf.d' ]
-      $packages            = [ 'nagios3', 'nagios-plugins' ]
-      $config_contact      = '/etc/nagios3/conf.d/contacts_puppet.cfg'
-      $config_contactgroup = '/etc/nagios3/conf.d/contactgroups_puppet.cfg'
-      $config_timeperiod   = '/etc/nagios3/conf.d/timeperiods_puppet.cfg'
+      $plugin_path          = '/usr/lib/nagios/plugins'
+      $eventhandler_path    = '/usr/share/nagios3/plugins/eventhandlers'
+      $nagios_cfg_path      = '/etc/nagios3/nagios.cfg'
+      $confdir              = '/etc/nagios3/conf.d'
+      $confdir_hosts        = '/etc/nagios3/conf.d/hosts'
+      $logdir               = '/var/log/nagios3'
+      $inotify_log          = '/var/log/nagios3/inotify.log'
+      $inotify_script       = '/usr/sbin/inotify-nagios'
+      $inotify_init         = '/etc/init.d/inotify-nagios'
+      $inotify_default      = '/etc/default/inotify-nagios'
+      $inotify_service_name = 'inotify-nagios'
+      $nagios_service_name  = 'nagios3'
+      $cfg_files            = [ '/etc/nagios3/commands.cfg' ]
+      $cfg_dirs             = [ '/etc/nagios-plugins/config', '/etc/nagios3/conf.d' ]
+      $packages             = [ 'nagios3', 'nagios-plugins', 'screen', 'inotify-tools' ]
+      $config_contact       = '/etc/nagios3/conf.d/contacts_puppet.cfg'
+      $config_contactgroup  = '/etc/nagios3/conf.d/contactgroups_puppet.cfg'
+      $config_timeperiod    = '/etc/nagios3/conf.d/timeperiods_puppet.cfg'
     }
     'RedHat': {
       $plugin_path         = $::architecture ? {
@@ -26,10 +32,12 @@ class nagios::params {
       $nagios_cfg_path     = '/etc/nagios/nagios.cfg'
       $confdir             = '/etc/nagios/conf.d'
       $confdir_hosts       = '/etc/nagios/conf.d/hosts'
+      $logdir              = '/var/log/nagios'
+      $inotify_log         = '/var/log/nagios/inotify.log'
       $nagios_service_name = 'nagios'
       $cfg_files           = [ '/etc/nagios/commands.cfg' ]
       $cfg_dirs            = [ '/etc/nagios-plugins/config', '/etc/nagios/conf.d' ]
-      $packages            = [ 'nagios', 'nagios-plugins' ]
+      $packages            = [ 'nagios', 'nagios-plugins', 'screen', 'inotify-tools' ]
       $config_contact      = '/etc/nagios/conf.d/contacts_puppet.cfg'
       $config_contactgroup = '/etc/nagios/conf.d/contactgroups_puppet.cfg'
       $config_timeperiod   = '/etc/nagios/conf.d/timeperiods_puppet.cfg'
